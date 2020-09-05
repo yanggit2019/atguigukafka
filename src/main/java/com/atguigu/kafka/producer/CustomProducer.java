04d3b6e50ba2cf42d7a1fca53593251b38a46a4a
yanggit2019 <838067203@qq.com>
2020-09-05 14:17:54 +0800

    一行代码展示

--- src/main/java/com/atguigu/kafka/producer/CustomProducer.java
+++ src/main/java/com/atguigu/kafka/producer/CustomProducer.java
@@ -1,10 +1,14 @@
 package com.atguigu.kafka.producer;
 
+import com.atguigu.kafka.interceptor.CounterInterceptor;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.omg.PortableInterceptor.Interceptor;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 public class CustomProducer {
@@ -18,6 +22,12 @@ public class CustomProducer {
         properties.put(ProducerConfig.ACKS_CONFIG,"all");
         properties.put(ProducerConfig.BATCH_SIZE_CONFIG,16384);
         properties.put(ProducerConfig.LINGER_MS_CONFIG,1);
+        List<String> interceptors = new ArrayList<>();
+        interceptors.add("com.atguigu.kafka.interceptor.CounterInterceptor");
+        interceptors.add("com.atguigu.kafka.interceptor.TimeInterceptor");
+        properties.put(ProducerConfig.INTERCEPTOR_CLASSES_CONFIG, interceptors);
+
+        
         //1.创建一个生产者对象
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
         //2.send方法
